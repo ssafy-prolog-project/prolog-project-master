c4823acbaff4f77714a34186c80e9e82b8e6f36a
defjin <kup90@naver.com>
2020-01-21 08:18:55 +0900

    change PostCard and add TopBar

--- mobx-with-react/src/components/PostList/PostCard/index.jsx
+++ mobx-with-react/src/components/PostList/PostCard/index.jsx
@@ -3,30 +3,29 @@ import styled from "styled-components";
 
 // data를 나중에 store로 옮기자. 어떤 자료를 가지고 있어야하지?
 // imageUrl, Title, category, text, likes, comments, created_at, updated_at , author
+// imageUrl이 none이면 그냥 타이틀을 키울까?
 // 일단 내부적으로 정의된 Post가 있고 이 Post의 DB 형태는 글을 작성하는 쪽에서 결정해야한다.
 // 여기에서는 요청 결과로 들어온 Post 결과물만을 보여주면 충분하다.
 const PostCard = ({ post }) => {
-  const title = post.title;
+  const { imgUrl, title, category, text, author, date } = post;
   const onClick = () => {
-    alert(title + "클릭했음");
+    alert(title + "클릭했음 나중에 페이지 옮기는 코드 넣자.");
   };
 
   return (
-    <CardMainLayOut onClick={onClick}>
-      <CardImage></CardImage>
+    <CardMainLayOut>
+      {imgUrl ? (
+        <CardImage onClick={onClick} src={imgUrl}></CardImage>
+      ) : (
+        <DefaultImage onClick={onClick}></DefaultImage>
+      )}
       <CardTitle>{title}</CardTitle>
-      <Date>3/2/2019</Date>
-      <CardDescription>
-        Green apples have a high fiber content which helps in increasing the
-        body's metabolism. While consuming an apple, make sure that you're not
-        tossing the peel in the trash. Consuming apple with its peel improves
-        the overall health. Due to its high fiber content, apple helps in
-        detoxification process. It keeps the liver and digestive system away
-        from harmful elements.
-      </CardDescription>
+      <Date>{date}</Date>
+      <CardDescription>{text}</CardDescription>
       <CardActionButton>0 Comments</CardActionButton>
       <CardActionButton>0 Likes</CardActionButton>
       <CardActionButton>0 Views</CardActionButton>
+      <p style={{ color: "black" }}>작성자 : {author}</p>
     </CardMainLayOut>
   );
 };
@@ -42,7 +41,21 @@ const CardMainLayOut = styled.div`
   border-color: black;
 `;
 
-const CardImage = styled.img``;
+const CardImage = styled.img`
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 5px;
+  width: 150px;
+`;
+
+const DefaultImage = styled.img`
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 5px;
+  width: 150px;
+  height: 150px;
+  background-color: black;
+`;
 
 const CardTitle = styled.h2`
   color: #fff;
--- mobx-with-react/src/components/PostList/index.jsx
+++ mobx-with-react/src/components/PostList/index.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
-import PostCard from './PostCard'
+import React, { useState } from "react";
+import PostCard from "./PostCard";
 
 const PostList = () => {
-    // posts 는 그냥 api 호출을 통해서가져오는 것 뿐이다. 그럼 그냥 Posts를 axios로 호출하는 코드와 
-    // 그것을 보여주는 코드면 충분하다. 
-    const posts = [
-        { title: 'test1'},
-        { title: 'test2'}
-    ]  // axios로 호출해서 받아오면 된다.
-    const postCards = posts.map(post => <PostCard post={post}/>)
-    return (
-        <div style={{ display: "flex"}}>
-            { postCards }
-        </div>
-    );
+  // posts 는 그냥 api 호출을 통해서가져오는 것 뿐이다. 그럼 그냥 Posts를 axios로 호출하는 코드와
+  // 그것을 보여주는 코드면 충분하다.
+  // imgUrl, title, category, text, author
+  const posts = [
+    {
+      imgUrl: "http://image.itdonga.com/files/2015/09/04/001.jpg",
+      title: "test1",
+      category: "post",
+      text : "Green apples have a high fiber content which helps in increasing the body's metabolism. While consuming an apple, make sure that you're not tossing the peel in the trash.",
+      author: "JEJ",
+      date : "3/2/2019"
+    },
+    {
+        imgUrl: "",
+        title: "test2",
+        category: "post",
+        text : "행복하세요. 오늘은 좋은 날이에요. 감사합니다. 여러분!!!?? 세상이 참 아름답죠?",
+        author: "JKY",
+        date : "3/2/2019"
+      },
+  ]; // axios로 호출해서 받아오면 된다.
+  const postCards = posts.map(post => <PostCard post={post} />);
+  return <div style={{ display: "flex" }}>{postCards}</div>;
 };
 
 export default PostList;
