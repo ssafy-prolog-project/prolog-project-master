92fdad7bca21887ceced356258b0ef52eee9041b
defjin <kup90@naver.com>
2020-01-21 17:57:57 +0900

    add dummy postStore use mobx

--- mobx-with-react/src/stores/index.js
+++ mobx-with-react/src/stores/index.js
@@ -1,10 +1,12 @@
 import CounterStore from './counter';
 import MarketStore from './market';
+import PostStore from './post';
 
 class RootStore {
   constructor() {
     this.counter = new CounterStore(this);
     this.market = new MarketStore(this);
+    this.post = new PostStore(this);
   }
 }
 
