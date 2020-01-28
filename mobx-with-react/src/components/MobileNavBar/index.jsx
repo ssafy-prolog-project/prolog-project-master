91471dce4860b766e3e2e86c9d2e1f3104e8582f
chokyungeun <ruddms0127@naver.com>
2020-01-28 09:41:18 +0900

    안쓰는 파일 정리

--- mobx-with-react/src/components/MobileNavBar/index.jsx
+++ mobx-with-react/src/components/MobileNavBar/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import MNavItems from './MNavItems';
 
 const MobileNavBar = () => {
+    
+
     return (
         <MNavBarLayout>
             <MNavItems></MNavItems>
