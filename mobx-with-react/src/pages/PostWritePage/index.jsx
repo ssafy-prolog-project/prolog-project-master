b5b7fdaeb02764f06a15168b11c7dcddd7eeb096
chokyungeun <ruddms0127@naver.com>
2020-01-29 18:08:44 +0900

    writepage

--- mobx-with-react/src/pages/PostWritePage/index.jsx
+++ mobx-with-react/src/pages/PostWritePage/index.jsx
@@ -5,6 +5,7 @@ import "react-quill/dist/quill.snow.css";
 import { Component } from "react";
 import styled from "styled-components";
 import { inject, observer } from "mobx-react";
+import { Editor, EditorState, RichUtils, convertToRaw } from "draft-js";
 
 export const RQuill = styled(ReactQuill)`
   width: 80%;
@@ -17,7 +18,7 @@ export const RQuill = styled(ReactQuill)`
 class PostWritePage extends Component {
   state = {
     quillbody: "",
-    draftbody: "",
+    editorState: EditorState.createEmpty(),
     quillmodules: {
       toolbar: [
         [{ header: [1, 2, 3, 4, 5, false] }],
@@ -44,7 +45,11 @@ class PostWritePage extends Component {
   };
 
   quillchangeEditor = e => this.setState({ quillbody: e });
-
+  onChange = editorState => {
+    this.setState({
+      editorState
+    });
+  };
   quillsave = () => {
     var contents = this.quillRef.getEditor().getContents();
     console.log(contents);
@@ -55,6 +60,41 @@ class PostWritePage extends Component {
   quillload = () => {
     const loadContents = this.props.testStore.quillbody;
     this.quillRef.getEditor().setContents(loadContents);
+  };
+  draftsave = () => {
+    const contentState = this.state.editorState.getCurrentContent();
+    console.log("content state", convertToRaw(contentState));
+    // this.setState({
+    //   editorState
+    // });
+  };
+
+  draftload = () => {
+    //const loadContents = this.props.testStore.quillbody;
+    //this.quillRef.getEditor().setContents(loadContents);
+  };
+  onChange = editorState => {
+    this.setState({ editorState });
+  };
+  handleKeyCommand = command => {
+    const newState = RichUtils.handleKeyCommand(
+      this.state.editorState,
+      command
+    );
+    if (newState) {
+      this.onChange(newState);
+      return "handled";
+    }
+    return "not-handled";
+  };
+  onUnderlineClick = () => {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+    );
+  };
+
+  onToggleCode = () => {
+    this.onChange(RichUtils.toggleCode(this.state.editorState));
   };
 
   render() {
@@ -74,7 +114,23 @@ class PostWritePage extends Component {
           <button onClick={this.quillload}>Load</button>
         </div>
 
-        <div id="draftEditor"></div>
+        <div
+          id="draftEditor"
+          style={{
+            paddingTop: "100px",
+            height: "400"
+          }}
+        >
+          <button onClick={this.onUnderlineClick}>Underline</button>
+          <button onClick={this.onToggleCode}>Code Block</button>
+          <Editor
+            editorState={this.state.editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.onChange}
+          />
+          <button onClick={this.draftsave}>Save</button>
+          <button onClick={this.draftload}>Load</button>
+        </div>
       </div>
     );
   }
