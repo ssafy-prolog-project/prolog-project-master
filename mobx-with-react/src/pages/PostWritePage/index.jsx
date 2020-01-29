0963a0d2e54896c12f21c5af909b7b79c69d9f0d
defjin <kup90@naver.com>
2020-01-29 16:47:05 +0900

    make testStore for test writing library

--- mobx-with-react/src/pages/PostWritePage/index.jsx
+++ mobx-with-react/src/pages/PostWritePage/index.jsx
@@ -4,44 +4,78 @@ import { ImageUploader } from "quill-image-upload";
 import "react-quill/dist/quill.snow.css";
 import { Component } from "react";
 import styled from "styled-components";
+import { inject, observer } from "mobx-react";
 
 export const RQuill = styled(ReactQuill)`
   width: 80%;
-  height: 100                                                                                                                                                                                                                                                                                                                                                                         %;
-
+  height: 100 %;
   cursor: pointer;
 `;
 
+@inject("testStore")
+@observer
 class PostWritePage extends Component {
   state = {
-    body: "",
-    modules: {
+    quillbody: "",
+    draftbody: "",
+    quillmodules: {
       toolbar: [
-        [{ header: [1, 2, 3, 4] }],
+        [{ header: [1, 2, 3, 4, 5, false] }],
         ["bold", "italic", "underline"],
-        ["image", "code-block"]
+        ["image", "video", "code-block"]
       ]
-    }
-    //   formats = [
-    //     'header', 'font', 'size',
-    //     'bold', 'italic', 'underline', 'strike', 'blockquote',
-    //     'list', 'bullet', 'indent',
-    //     'link', 'image', 'video'
-    //   ]
+    },
+    quillformats: [
+      "header",
+      "font",
+      "size",
+      "bold",
+      "italic",
+      "underline",
+      "strike",
+      "blockquote",
+      "list",
+      "bullet",
+      "indent",
+      "link",
+      "image",
+      "video"
+    ]
   };
 
-  changeEditor = e => this.setState({ body: e });
+  quillchangeEditor = e => this.setState({ quillbody: e });
+
+  quillsave = () => {
+    var contents = this.quillRef.getEditor().getContents();
+    console.log(contents);
+    this.props.testStore.quillbody = contents;
+    console.log(this.state.quillbody);
+  };
+
+  quillload = () => {
+    const loadContents = this.props.testStore.quillbody;
+    this.quillRef.getEditor().setContents(loadContents);
+  };
 
   render() {
     return (
-      <RQuill
-        ref={el => (this.quillRef = el)}
-        value={this.state.body} // state 값
-        onChange={this.changeEditor}
-        modules={this.modules}
-        formats={this.formats}
-        placeholder={"아무거나 입력해 주세요"}
-      />
+      <div>
+        <div id="quillEditor">
+          <ReactQuill
+            name="editor"
+            ref={el => (this.quillRef = el)}
+            value={this.state.quillbody} // state 값
+            onChange={this.quillchangeEditor}
+            modules={this.quillmodules}
+            formats={this.quillformats}
+            placeholder={"아무거나 입력해 주세요"}
+          />
+          <button onClick={this.quillsave}>Save</button>
+          <button onClick={this.quillload}>Load</button>
+        </div>
+
+        <div id="draftEditor"></div>
+      </div>
     );
   }
 }
