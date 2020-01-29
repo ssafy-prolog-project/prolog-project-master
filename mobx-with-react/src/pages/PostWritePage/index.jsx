7fc34f8495a93799844a693807647bc787dacbed
chokyungeun <ruddms0127@naver.com>
2020-01-29 14:16:31 +0900

    commit

--- mobx-with-react/src/pages/PostWritePage/index.jsx
+++ mobx-with-react/src/pages/PostWritePage/index.jsx
@@ -3,6 +3,14 @@ import ReactQuill, { Quill } from "react-quill";
 import { ImageUploader } from "quill-image-upload";
 import "react-quill/dist/quill.snow.css";
 import { Component } from "react";
+import styled from "styled-components";
+
+export const RQuill = styled(ReactQuill)`
+  width: 80%;
+  height: 100                                                                                                                                                                                                                                                                                                                                                                         %;
+
+  cursor: pointer;
+`;
 
 class PostWritePage extends Component {
   state = {
@@ -26,10 +34,9 @@ class PostWritePage extends Component {
 
   render() {
     return (
-      <ReactQuill
+      <RQuill
         ref={el => (this.quillRef = el)}
         value={this.state.body} // state 값
-        theme="snow" // 테마값 이미 snow.css를 로드해서 제거해도 무망
         onChange={this.changeEditor}
         modules={this.modules}
         formats={this.formats}
