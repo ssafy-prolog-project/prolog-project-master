6183acea68340d2d4abf3b9b7542767cc1686ffa
chokyungeun <ruddms0127@naver.com>
2020-01-29 12:17:56 +0900

    글쓰기 페이지

--- mobx-with-react/src/pages/PostWritePage/index.jsx
+++ mobx-with-react/src/pages/PostWritePage/index.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import ReactQuill, { Quill } from "react-quill";
+import { ImageUploader } from "quill-image-upload";
+import "react-quill/dist/quill.snow.css";
+import { Component } from "react";
 
-const PostWritePage = () => {
+class PostWritePage extends Component {
+  state = {
+    body: "",
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, 3, 4] }],
+        ["bold", "italic", "underline"],
+        ["image", "code-block"]
+      ]
+    }
+    //   formats = [
+    //     'header', 'font', 'size',
+    //     'bold', 'italic', 'underline', 'strike', 'blockquote',
+    //     'list', 'bullet', 'indent',
+    //     'link', 'image', 'video'
+    //   ]
+  };
+
+  changeEditor = e => this.setState({ body: e });
+
+  render() {
     return (
-        <div>
-            <h1>이거는글쓰는페이지예요</h1>
-        </div>
+      <ReactQuill
+        ref={el => (this.quillRef = el)}
+        value={this.state.body} // state 값
+        theme="snow" // 테마값 이미 snow.css를 로드해서 제거해도 무망
+        onChange={this.changeEditor}
+        modules={this.modules}
+        formats={this.formats}
+        placeholder={"아무거나 입력해 주세요"}
+      />
     );
-};
+  }
+}
 
 export default PostWritePage;
