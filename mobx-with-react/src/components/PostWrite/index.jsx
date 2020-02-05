f29e7984114cdeca22668d2841f5e9aa65d51926
조경은 <ruddms0127@naver.com>
2020-02-05 13:48:07 +0900

    Client: WritePage 임시저장

--- mobx-with-react/src/components/PostWrite/index.jsx
+++ mobx-with-react/src/components/PostWrite/index.jsx
@@ -6,24 +6,30 @@ import { Underline} from "styled-icons/boxicons-regular/Underline";
 import { CodeBlock } from "styled-icons/boxicons-regular/CodeBlock";
 import { FormatBold } from "styled-icons/material/FormatBold";
 import { FormatItalic } from "styled-icons/material/FormatItalic";
+import { Link } from "styled-icons/boxicons-regular/Link";
+
+export const LinkIcon = styled(Link)`
+cursor: pointer;
+  width: 30px;
+`;
 
 export const ItalicIcon = styled(FormatItalic)`
 cursor: pointer;
-  width: 40px;
+  width: 30px;
 `;
 
 export const BoldIcon = styled(FormatBold)`
 cursor: pointer;
-  width: 40px;
+  width: 30px;
 `;
 export const UnderlineIcon = styled(Underline)`
    cursor: pointer;
-  width: 40px;
+  width: 30px;
 `;
 
 export const CodeBlockIcon = styled(CodeBlock)`
 cursor: pointer;
-  width: 40px;
+  width: 30px;
 `;
 
 class PostWrite extends Component {
@@ -31,12 +37,14 @@ class PostWrite extends Component {
         super();
         this.state = {
           editorState: EditorState.createEmpty(),
+          showLinkInput: false
         };
       }
       onChange = (editorState) => {
         this.setState({ editorState });
       };
       handleKeyCommand = (command) => {
+
         const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
         if (newState) {
           this.onChange(newState);
@@ -48,6 +56,18 @@ class PostWrite extends Component {
         this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
       }
     
+      onLinkClick = () => {
+        this.setState({
+          showLinkInput: true
+        });
+      }
+      
+      onLinkInputClose = () => {
+        this.setState({
+          showLinkInput: false
+        });
+      }
+
       onToggleCode = () => {
         this.onChange(RichUtils.toggleCode(this.state.editorState));
       }
@@ -63,27 +83,33 @@ class PostWrite extends Component {
           const { classes } = this.props;
         return (
             <PostWriteLayout>
-              
-           
+                <TextPlugin>
+                <TIcons>
+                      <UnderlineIcon onClick={this.onUnderlineClick}></UnderlineIcon>
+                    </TIcons>
+                    <TIcons>
+                        <BoldIcon onClick={this.onBoldClick}></BoldIcon>
+                    </TIcons>
+                    <TIcons>
+                        <ItalicIcon onClick={this.onItalicClick}></ItalicIcon>
+                    </TIcons>
+                </TextPlugin>
                 <Editor
                   editorState={this.state.editorState}
                   handleKeyCommand={this.handleKeyCommand}
                   onChange={this.onChange}
                   placeholder="내용을 입력해주세요..."
+                  ref="editor"
+                  spellCheck="true"
                 />
                 <PluginSpace>
-                    <Icons>
-                    <UnderlineIcon onClick={this.onUnderlineClick}></UnderlineIcon>
-                
-                    </Icons>
+                    
                     <Icons>
                         <CodeBlockIcon onClick={this.onToggleCode}></CodeBlockIcon>
                     </Icons>
+                    
                     <Icons>
-                        <BoldIcon onClick={this.onBoldClick}></BoldIcon>
-                    </Icons>
-                    <Icons>
-                        <ItalicIcon onClick={this.onItalicClick}></ItalicIcon>
+                        <LinkIcon onClick={this.onLinkClick}></LinkIcon>
                     </Icons>
                 
                 </PluginSpace>
@@ -98,17 +124,30 @@ class PostWrite extends Component {
       
 }
 
+  const TextPlugin = styled.div`
+width: 500px;
+height: 35px;
+background-color: #f2f2f2;
+display: none;
 
+`;
 
-
+const TIcons = styled.div`
+    align-content: center;
+    width: 35px;
+    float: left;
+    :hover {
+        opacity: 50%;
+  }
+`;
 const PostWriteLayout = styled.div`
-    
+    background-color: white;
 `;
 
 
 const PluginSpace = styled.div`
     float: right;
-    width: 4rem;
+    width: 40px;
     height: 10rem;
     background-color: white;
     margin-top: 6rem;
@@ -116,7 +155,7 @@ const PluginSpace = styled.div`
 `;
 const Icons = styled.div`
 
-    width: 40px;
+    width: 30px;
     float: right;
     margin-top: 2rem;
     :hover {
