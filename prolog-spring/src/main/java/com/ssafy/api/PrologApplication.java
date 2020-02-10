1a7a5c8274233ada7a7d8bb72e6b07a378fb1cc7
류일한 <lyuilhan@ip-70-12-228-162.kscymo.spcsdns.net>
2020-02-10 17:54:01 +0900

    Server : add jpa auto create datetime annotation(@EnableJpaAuditing)

--- prolog-spring/src/main/java/com/ssafy/api/PrologApplication.java
+++ prolog-spring/src/main/java/com/ssafy/api/PrologApplication.java
@@ -3,6 +3,7 @@ package com.ssafy.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @CrossOrigin("*")
+@EnableJpaAuditing
 public class PrologApplication {
 
     public static void main(String[] args) {
