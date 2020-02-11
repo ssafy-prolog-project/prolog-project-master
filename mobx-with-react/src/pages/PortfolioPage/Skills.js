bef30b0f8cd5aa16d4455dd020c99fe3f5b66bfc
이인경 <inky94@naver.com>
2020-02-11 15:39:20 +0900

    client: porfolio edit update

--- mobx-with-react/src/pages/PortfolioPage/Skills.js
+++ mobx-with-react/src/pages/PortfolioPage/Skills.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import {Edit} from 'styled-icons/boxicons-regular/Edit';
+
+export const EditP = styled(Edit)`
+    color: #DCDCDC;
+    width: 1rem;
+    height: 1rem;
+    margin-bottom: 2.4rem;
+`
 
 class Skills extends Component{
     render(){
         return(
             <SkillsLayout>
-                <SkTitle>Skills</SkTitle>
+                <SkTitle>Skills<EditP/></SkTitle>
                 <SkContent>
                 But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?
                 </SkContent>
@@ -29,6 +37,7 @@ const SkTitle = styled.div`
     text-align: center;
     font-size: 2.4rem;
 `
+
 const SkContent = styled.div`
     text-align:center;
     padding: 5%;
