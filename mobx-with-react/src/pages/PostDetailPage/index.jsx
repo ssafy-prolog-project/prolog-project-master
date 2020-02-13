9c97cf383ecf87ecc095c9234ca118bea8199ca0
조경은 <ruddms0127@naver.com>
2020-02-13 09:34:40 +0900

    Common: git commit

--- mobx-with-react/src/pages/PostDetailPage/index.jsx
+++ mobx-with-react/src/pages/PostDetailPage/index.jsx
@@ -64,7 +64,7 @@ class PostDetailPage extends Component {
           <Link to={"/"} style={{ textDecoration: "none" }}>
             <MLogo>Prolog;</MLogo>
           </Link>
-          <UserButton></UserButton>
+          <DetailUserButton></DetailUserButton>
         </PostViewHeader>
         <PostContentWrapper>
           <Left>Left</Left>
@@ -101,108 +101,11 @@ export const LINKS = styled(Link)`
   
 `;
 
-const LoginButton = styled.div`
-  border-style: solid;
-  border-width: 1.5px;
-  width: 5rem;
-  height: 2rem;
-  float: right;
-  margin-right: 2rem;
-  border-color: white;
-  margin-top: 0.5rem;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+export const DetailUserButton = styled(UserButton)`
+  
 `;
 
-const SelectMenus = styled.div`
-  display: none;
-  position: relative;
-  background-color: #f1f1f1;
-  min-width: 100px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  z-index: 1;
-  &:hover .menubar {
-    display: block;
-  }
-  margin-right: 50rem;
-  margin-top: 3.2rem;
-  @media (max-width: 768px) {
-    right: 3rem;
-  }
 
-  @media (min-width: 768px) and (max-width: 1024px) {
-    left: -150px;
-    top: 0%;
-  }
-  /*display: none;
-  margin-top: -1rem;
-  float: right;
-  margin-right: 3rem;
-   position: absolute; */
-  /*background-color: #f9f9f9;
-  min-width: 100px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 3;*/
-`;
-const SelectMenu = styled.a`
-  color: black;
-  padding: 10px 12px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-
-  /* position: absolute; */
-  /* color: black;
-  padding: 10px 12px;
-  display: block;
-  text-align: left;
-  position: absolute; */
-  cursor: pointer;
-  :hover {
-    background-color: #b0b0b0;
-  }
-`;
-const Img = styled.div`
-  position: relative;
-  display: inline-block;
-  grid-area: test;
-
-  padding-top: 1rem;
-  margin-left: 85%;
-  &:hover .menubar {
-    display: block;
-  }
-  @media (max-width: 768px) {
-    padding-left: 0;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    padding-left: 10%;
-  }
-`;
-
-const ProfileImg = styled.img`
-  width: 3rem;
-  height: 3rem;
-  object-fit: cover;
-  border-radius: 50%;
-  cursor: pointer;
-  float: right;
-  margin-right: 3rem;
-  z-index: 2;
-  &:hover .menubar {
-    display: block;
-  }
-  @media (max-width: 768px) {
-    margin-right: 10%;
-  }
-
-  @media (min-width: 768px) and (max-width: 1024px) {
-    margin-right: 1rem;
-  }
-`;
 
 const PostDetailPageLayout = styled.div`
   flex-direction: row;
