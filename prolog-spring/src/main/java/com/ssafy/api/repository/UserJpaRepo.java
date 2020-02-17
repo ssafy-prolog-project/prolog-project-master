36b30edb9b4535a5e58c4b252808093b5ad0910a
류일한 <lyuilhan@ip-70-12-228-162.kscymo.spcsdns.net>
2020-02-17 16:52:51 +0900

    Server : userJpaRepository dependency bug fix

--- prolog-spring/src/main/java/com/ssafy/api/repository/UserJpaRepo.java
+++ prolog-spring/src/main/java/com/ssafy/api/repository/UserJpaRepo.java
@@ -1,7 +1,6 @@
 package com.ssafy.api.repository;
 
 import com.ssafy.api.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
