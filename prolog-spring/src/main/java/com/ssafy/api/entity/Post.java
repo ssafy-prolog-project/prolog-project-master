3f6b4c84ce3ca10d6d27d5dbb6c4166bd6e6e5ae
류일한 <lyuilhan@ip-70-12-228-162.kscymo.spcsdns.net>
2020-02-18 18:48:01 +0900

    Server : edit post, comment entity @data annotation -> @Getter, @Setter

--- prolog-spring/src/main/java/com/ssafy/api/entity/Post.java
+++ prolog-spring/src/main/java/com/ssafy/api/entity/Post.java
@@ -1,10 +1,7 @@
 package com.ssafy.api.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -12,7 +9,8 @@ import javax.persistence.*;
 import java.util.Arrays;
 import java.util.List;
 
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
