b38a658f7d41d1a0382f6b81e42722d2d4390c57
조경은 <ruddms0127@naver.com>
2020-02-18 17:19:25 +0900

    Client: agent update 수정

--- mobx-with-react/src/agent.js
+++ mobx-with-react/src/agent.js
@@ -82,11 +82,8 @@ const Posts = {
   
   create: post =>{
     // if(window.sessionStorage.getItem("jwt")==null || window.sessionStorage.getItem("jwt")=="") new Error("로그인 하러 가자!")
-
     requests.post('/post', {"title":post.title, "coverColor":post.coverColor, "coverImage":post.coverImage, "body": post.body, "tagList": post.tagList}, {"X-AUTH-TOKEN": window.sessionStorage.getItem("jwt")})
   
-
-    
   },
   update: post => 
   requests.put(`/post/${post.id}`, {post}),
