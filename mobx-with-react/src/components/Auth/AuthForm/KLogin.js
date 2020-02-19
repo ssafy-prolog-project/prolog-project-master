ab6994d0b06558515201267fea410efd53fafef9
이인경 <inky94@naver.com>
2020-02-19 10:59:19 +0900

    client: login edit

--- mobx-with-react/src/components/Auth/AuthForm/KLogin.js
+++ mobx-with-react/src/components/Auth/AuthForm/KLogin.js
@@ -39,7 +39,7 @@ class KLogin extends Component {
         this.props.authStore.setProfileimg(this.props.authStore.user_detail.picture);
         this.props.authStore.setName(this.props.authStore.user_detail.name);
         this.props.authStore.setEmail(this.props.authStore.user_detail.email);
-        //window.location.replace("http://localhost:3000/");
+        window.location.replace("http://localhost:3000/");
         // 현재는 cors 에러 나서 실패했는데도 로그인 성공으로 찍어버림.
       })
       .catch(err => {
